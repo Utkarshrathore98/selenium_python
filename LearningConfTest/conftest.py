6465089e1c8bf314064dce55d26ee307230f4c43
Utkarsh rathore <rathoreutkarsh1998@gmail.com>
2023-11-08 21:57:25 +0530

    Fifth Commit

--- LearningConfTest/conftest.py
+++ LearningConfTest/conftest.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.fixture(scope="session", autouse=True)
-# if you are using older version, you can use @pytest.yield_fixture().
-# If autouse = True , you do not have to mention the module name (tc_setup) in the module parameter. .i.e in test_conftestLearning.py.
-# If we don't mention the scope then by default scope will be function.
-# If scope ="session" then tier down will only execute once all the modules are executed.
+# if you are using older version, you can use @pytest.yield_fixture(). If autouse = True , you do not have to mention
+# the module name (tc_setup) in the module parameter. .i.e in test_conftestLearning.py. If we don't mention the scope
+# then by default scope will be function. If scope ="session" then tier down will only execute once all the modules
+# are executed.
 def tc_setUp():
     print("Launch browser")
     print("Login")
